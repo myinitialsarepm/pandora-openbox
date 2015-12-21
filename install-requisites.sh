f6f4d33bb9ca663f119793dfa0dc97062284e48d
Peter Morris <peter@localhost.localdomain>
2015-12-21 21:55:53 +0000

    Initial commit.

---
+++ install-requisites.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo dnf -qy install \
+	lightdm \
+	lightdm-gtk-greeter \
+	lightdm-gtk-greeter-settings \
+	openbox \
+	xcompmgr \
+	obconf \
+	tint2 \
+	feh \
+	conky \
+	notification-daemon
